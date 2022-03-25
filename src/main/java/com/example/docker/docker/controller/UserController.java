cdb0d1c6f5996b6e7ab254e4b1ad82a262715308
dipu11 <dipubuet2@gmail.com>
2022-03-25 21:32:12 +0600

    disintegrating mysql from project

--- src/main/java/com/example/docker/docker/controller/UserController.java
+++ src/main/java/com/example/docker/docker/controller/UserController.java
@@ -1,11 +1,8 @@
 package com.example.docker.docker.controller;
 
 import com.example.docker.docker.model.User;
-import com.example.docker.docker.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
 
 /**
  * Created by DIPU on 12/6/21
@@ -14,22 +11,17 @@ import java.util.Optional;
 @RequestMapping("/api/user")
 public class UserController {
 
-    @Autowired
-    private UserRepository userRepository;
 
     @PostMapping
     public User saveUser(@RequestBody User user)
     {
-        user=userRepository.save(user);
         return user;
     }
 
     @GetMapping("/{userId}")
     public User getUserById(@PathVariable("userId") Long userId)
     {
-        Optional<User> user= userRepository.findById(userId);
-
-        return user.isPresent()? user.get(): null;
+        return new User(1L, "dummy", "dummy", 25, "english");
     }
 
 }
